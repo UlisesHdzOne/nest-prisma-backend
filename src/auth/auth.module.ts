0131a168c71af4c2ed84fc21ee9c2f1830783b5e
ulisesHdz <ulisesdejesusk9999@gmail.com>
2025-06-14 15:47:01 -0600

    feat(auth): configurar JwtModule con variables de entorno y fix tsconfig.json en Docker

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { PrismaModule } from '../prisma/prisma.module'; // Importa PrismaModule
-
+import { PrismaModule } from '../prisma/prisma.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [PrismaModule, 
-  JwtModule.register({ 
-      secret: 'mi_clave_secreta',  // cámbiala por algo seguro
-      signOptions: { expiresIn: '1h' }, // token dura 1 hora
-   })],
+  imports: [
+    PrismaModule,
+    ConfigModule,  
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
